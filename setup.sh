224c21c9d734d396b1891b6c753440d0166187e4
pahasara-ridmaka <pahasara.ridmaka95@gmail.com>
2026-07-23 17:06:46 +0530

    Added .zshrc and update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,3 +7,9 @@ echo "Creating symlink for .bashrc..."
 ln -s $(pwd)/.bashrc ~/.bashrc
 
 echo "Dotfiles setup completed successfully!"
+
+echo "Remeving exustubg .zshrc..."
+rm -f ~/.zshrc
+
+echo "Creating symlink for .zshrc..."
+ln -s $(pwd)/.zshrc ~/.zshrc
