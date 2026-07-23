6cba9aec4140dfa27a4fb40912a5504de8eea382
pahasara-ridmaka <pahasara.ridmaka95@gmail.com>
2026-07-23 17:27:57 +0530

    fix: setup syntax error in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Installing Oh My Zsh
-if [! -d "$HOME/.oh-my-zsh" ]; then
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
   echo "Oh My Zsh is not installed. Installing Oh My Zsh..."
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 else
